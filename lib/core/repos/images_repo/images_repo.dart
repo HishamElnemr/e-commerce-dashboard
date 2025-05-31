b3addc7aa7be122e4deb8dd6189e33e5e0a38284
Hisham Ahmed <hishamcom48@gmail.com>
2025-05-31 11:15:33 +0300

    Add images repol

---
+++ lib/core/repos/images_repo/images_repo.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_dashboard/core/errors/failures.dart';
+
+abstract class ImagesRepo 
+{
+  Future <Either<Failure, String>> uploadImage(File imagePath);
+
+}
--- lib/core/repos/product_repo/product_repo_impl.dart
+++ lib/core/repos/product_repo/product_repo_impl.dart
@@ -1,4 +1,4 @@
-import 'package:fruits_hub_dashboard/core/repos/product_repo.dart';
+import 'package:fruits_hub_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruits_hub_dashboard/features/add_product/domain/entities/add_product_entity_input.dart';
 
 class ProductRepoImpl implements ProductRepo {
