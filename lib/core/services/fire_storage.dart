8fb1351af5fac4d69ce897743b9b7c5066182bc9
Hisham Ahmed <hishamcom48@gmail.com>
2025-05-31 11:49:44 +0300

    create FireStorage code

--- lib/core/services/fire_storage.dart
+++ lib/core/services/fire_storage.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/storage_services.dart';
+import 'package:path/path.dart' as path;
 
 class FireStorage implements StorageServices {
-  final storageRefrence = FirebaseStorage.instance;
+  final storageRefrence = FirebaseStorage.instance.ref();
   @override
-  Future<String> uploadFile(String file) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String filePath) async {
+    String fileName = path.basename(file.path);
+    String extensionName = path.extension(file.path);
+    var fileRef = storageRefrence.child('$filePath/$fileName.$extensionName');
+    await fileRef.putFile(file);
+    var fileUrl = await fileRef.getDownloadURL();
+    return fileUrl;
   }
 }
