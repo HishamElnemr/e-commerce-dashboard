4dbf092daee25b82b8ec090400dd7ab70d71fd09
Hisham Ahmed <hishamcom48@gmail.com>
2025-06-01 10:22:34 +0300

    edit getit

--- lib/core/services/getit_services.dart
+++ lib/core/services/getit_services.dart
@@ -2,15 +2,18 @@ import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo_impl.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/product_repo_impl.dart';
+import 'package:fruits_hub_dashboard/core/services/database_services.dart';
 import 'package:fruits_hub_dashboard/core/services/fire_storage.dart';
+import 'package:fruits_hub_dashboard/core/services/firestore_services.dart';
 import 'package:fruits_hub_dashboard/core/services/storage_services.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
 void setup() {
   getIt.registerSingleton<StorageServices>(FireStorage());
+  getIt.registerSingleton<DatabaseService>(FirestoreServices());
   getIt.registerSingleton<ImagesRepo>(
     ImagesRepoImpl(storageServices: getIt.get<StorageServices>()),
   );
-  getIt.registerSingleton<ProductRepo>(ProductRepoImpl());
+  getIt.registerSingleton<ProductRepo>(ProductRepoImpl(databaseService: getIt.get<DatabaseService>()));
 }
